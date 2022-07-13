c5a85adfe7866bfcfa3c39611a51ac232a6811f1
Diego Lopes Mercado <diegolm@ciandt.com>
2022-07-13 16:03:12 -0300

    Cafeteria

--- 0x01/cafeteria/Bebida.java
+++ 0x01/cafeteria/Bebida.java
@@ -1,2 +1,14 @@
-public class Bebida {
+import java.util.List;
+import java.util.Locale;
+
+public abstract class Bebida {
+
+    public abstract List<String> obterIngredientes();
+
+    public abstract double obterPreco();
+
+    @Override
+    public String toString() {
+        return String.format(Locale.FRANCE,"Preco: %.2f - Ingredientes: %s", obterPreco(), obterIngredientes());
+    }
 }
